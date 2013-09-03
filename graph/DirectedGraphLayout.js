8dc64908b9ae87d873c463f9bbe5fd61ae8d7548
Simon Speich <simon.speich@gmail.com>
2013-09-03 19:13:27 +0200

    fixed wrong/inconsistent name in license

--- graph/DirectedGraphLayout.js
+++ graph/DirectedGraphLayout.js
@@ -1,13 +1,13 @@
 /**
  * @license
- * This file is part of DirectedGraph Application.
+ * This file is part of the DirectedGraph library.
  *
- * NAFIDAS Web Map Application is free software: you can redistribute it and/or modify
+ * The DirectedGraph library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * DirectedGraph Application is distributed in the hope that it will be useful,
+ * The DirectedGraph library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
